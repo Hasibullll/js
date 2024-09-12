9fff44d2d9af97fd8cf22c3badfb682e38e2fa0f
Hasibullll <hasibulislam.bracu@gmail.com>
2024-09-13 05:01:50 +0600

    first commit

---
+++ scripts/alpha.js
@@ -0,0 +1,13 @@
+function play() {
+    
+    // const page_1= document.getElementById('home_src');
+    // page_1.classList.add('hidden');
+
+
+    // const page_2 =document.getElementById('playground');
+    // page_2.classList.remove('hidden');
+    
+    
+    hideItem('home_src');
+    showItem('playground');
+}
---
+++ scripts/alpha_2.js
@@ -0,0 +1,18 @@
+
+
+
+function hideItem(elementID) {
+    const element = document.getElementById(elementID);
+    element.classList.add('hidden');
+
+    
+}
+
+function showItem(elementID) {
+    const element = document.getElementById(elementID);
+    console.log(element);
+    
+   // element.classList.remove('hidden');
+
+    
+}
